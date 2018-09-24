7b30c5bcf9400c1d3d61e56b8dcaa4eacd9b72f4
martypdx <marty@martypdx.com>
2018-09-24 11:57:55 -0700

    remove unused console.log

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const { greet } = require('./lib/pleasantries');
 
 http.createServer((req, res) => {
     const name = req.url.split('/')[1];
-    console.log('>>' + name + '<<', typeof name);
     const greeting = greet(name);
     res.end(greeting);
 }).listen(3000);
